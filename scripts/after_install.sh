a80198836ca0af89ba09e2da8f4e34c6aff02b53
commGom <hyunwoo921129@gmail.com>
2022-12-09 15:00:02 +0900

    after_install.sh modified

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,7 +2,7 @@
 echo 'run after_install.sh: ' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline-test/deploy.log
 
 echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline-test/deploy.log
-cd /home/ec2-user/nodejs-aws-codedeploy-pipeline >> /home/ec2-user/nodejs-aws-codedeploy-pipeline-test/deploy.log
+cd /home/ec2-user/nodejs-aws-codedeploy-pipeline-test >> /home/ec2-user/nodejs-aws-codedeploy-pipeline-test/deploy.log
 
 echo 'npm install' >> /home/ec2-user/nodejs-aws-codedeploy-pipeline-test/deploy.log 
 npm install >> /home/ec2-user/nodejs-aws-codedeploy-pipeline-test/deploy.log
